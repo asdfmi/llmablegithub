c36deadf07d44dc7235a0bbe47f352c87df37cab
asdfmi <nattogohan710580@gmail.com>
2025-09-02 18:24:50 +0900

    src/index: fix hung

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import { buildRouter } from "./routes/router";
 export default {
   async fetch(request: Request): Promise<Response> {
     const router = buildRouter();
-    return await router['handle'](request);
+    return await router.fetch(request);
   },
 };
